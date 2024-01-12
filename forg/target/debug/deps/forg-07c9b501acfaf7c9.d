5243a44a545968b57e1b0a5a2fec76fad82ccb6d
nam2ee <nyj5404@yonsei.ac.kr>
2024-01-12 21:18:01 +0900

    forg

--- forg/target/debug/deps/forg-07c9b501acfaf7c9.d
+++
@@ -1,5 +0,0 @@
-/Users/yoonjae/RustyDay/forg/target/debug/deps/forg-07c9b501acfaf7c9: src/main.rs
-
-/Users/yoonjae/RustyDay/forg/target/debug/deps/forg-07c9b501acfaf7c9.d: src/main.rs
-
-src/main.rs:
--- forg/target/debug/forg.d
+++
@@ -1,1 +0,0 @@
-/Users/yoonjae/RustyDay/forg/target/debug/forg: /Users/yoonjae/RustyDay/forg/src/main.rs
